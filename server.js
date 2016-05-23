0ef943e119289c37129db7709e499e5145631d4e
monasingh27 <mona.singh@workscripts.in>
2016-05-23 16:48:04 +0530

    sockets.io

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var PORT =3000;
-var express =require('express');
-var app=express();
-var http =require('http').Server(app);
-app.use(express.static(__dirname+ '/public'));
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-http.listen(PORT, function ()
-{
-	console.log('server started at: '+PORT);
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function () {
+	console.log('User connected via socket.io!');
+});
+
+http.listen(PORT, function () {
+	console.log('Server started!');
 });
