7e6a9e23b721041c67f1009812ddc9d4b51d3bfb
monasingh27 <mona.singh@workscripts.in>
2016-05-23 17:51:48 +0530

    socket.io

--- public/js/app.js
+++ public/js/app.js
@@ -5,3 +5,9 @@ socket.on('connect',function()
 	 console.log('connected to socket io server');
 
 });
+
+socket.on('message',function(message)
+{
+    console.log('new message:');
+    console.log(message.text);
+});
