379c3301db962c1e3ff593dde5fc2c07a12a064e
monasingh27 <mona.singh@workscripts.in>
2016-06-08 17:37:20 +0530

    chat app

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,81 @@
+var name =getQueryVariable('name') || 'Anonymous';
+// var room =getQueryVariable('room');
+console.log(name);
+console.log(typeof name);
+
+var room;
+
+// console.log(typeof room);
+
 var socket = io();
+
+// console.log(name + ' wants to join '+ room);
+
+if(name.substring( name.indexOf('@'))=='@gmail.com')
+ {
+  
+   room= 'GmailRoom';
+  
+ console.log(room);
+
+  console.log('gmail.com')
+
+ 
+ jQuery('.room-title').text(room);
+  
+}
+else if(name.substring( name.indexOf('@'))=='@workscripts.in')
+{
+
+     room= 'WorkscriptsRoom';
+  
+ console.log(room);
+
+  console.log('workscripts.com')
+
+ 
+ jQuery('.room-title').text(room);
+
+}
+else
+{
+
+  room='GeneralRoom';
+
+ console.log(room);
+
+  console.log('General')
+
+ 
+ jQuery('.room-title').text(room);
+
+}
+
+console.log(name + ' wants to join '+ room);
 
 socket.on('connect',function()
 {
 	 console.log('connected to socket io server');
+      socket.emit('joinRoom',{
+
+        name: name,
+        room: room
+      });
+
 
 });
 
 socket.on('message',function(message)
-{
+{ 
+	  var momentTimestamp = moment.utc(message.timestamp);
+	  var $message = jQuery('.messages');
+
     console.log('new message:');
     console.log(message.text);
 
-    jQuery('.messages').append('<p>' + message.text + '</p>');
+
+  $message.append('<p><strong>'+ message.name + ' ' + momentTimestamp.local().format('h:mm a') +'</strong</p>')
+   $message.append('<p>'+message.text +'</p>')
 });
 
 var $form = jQuery('#message-form');
@@ -19,8 +83,14 @@ var $form = jQuery('#message-form');
 $form.on('submit', function(event)
 {
   event.preventDefault();
+
+  var $message = $form.find('input[name=message]');
+
   socket.emit('message',{
-  	text: $form.find('input[name=message]').val()
-  })
+  	name: name,
+  	text: $message.val()
+  });
+
+$message.val('');
 
 });
